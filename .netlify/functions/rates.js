9235ad4e5b7c9bb860ec8a76581bfaa346932635
Blair Anderson <blair81@gmail.com>
2019-07-22 16:14:42 -0700

    update the format of this request to include a count

--- .netlify/functions/rates.js
+++ .netlify/functions/rates.js
@@ -18,7 +18,7 @@ async function handler(event, context) {
         Accept: "application/json"
       }
     });
-    
+
     console.log("fetched " + URL);
     console.log(response);
 
@@ -64,7 +64,7 @@ async function handler(event, context) {
         url: "https://trendapi.org/forecast",
         method: "post",
         headers: { Accept: "application/json" },
-        data: JSON.stringify(historical)
+        data: JSON.stringify({series: historical, count: 14})
       });
       resp.chart.push({ name: "Forecast", data: forecast.data.series })
     } catch (e) {
